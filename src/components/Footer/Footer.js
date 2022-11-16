9cc3ef5b2016871485c01fcb6201ee43871c3a9a
Mariah <mariahjohnston217@outlook.com>
2022-11-15 23:14:04 -0500

    started working on footer, added github and linked in icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 
 export default function Footer() {
     return (
-        <>
-            
-        </>
+        <h1>
+            <FaGithub /> <FaLinkedin />
+        </h1>
+
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Disclosure, Menu } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { Route } from 'react-router-dom';
 
 const navigation = [
   { name: 'About Me', href: '/', current: true },
